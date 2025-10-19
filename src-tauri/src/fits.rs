dfe2aada60077b15aa1fabd489c853a4dfa8fe38
Christian Brendlin <ch@brendlinonline.de>
2025-10-19 21:06:05 +0200

    Initial Commit!

--- src-tauri/src/fits.rs
+++ src-tauri/src/fits.rs
@@ -1,1 +1,31 @@
-use fitsio::FitsFile;
+use anyhow::{ensure, Result};
+use fitsio::{hdu::HduInfo, FitsFile};
+
+pub struct FitsImage {
+    pub data: Vec<f32>,
+    pub width: usize,
+    pub height: usize,
+}
+
+pub fn load_fits_f32(path: &str) -> Result<FitsImage> {
+    let mut f = FitsFile::open(path)?;
+    let hdu = f.primary_hdu()?;
+
+    // Safely match and borrow shape info
+    let shape = match &hdu.info {
+        HduInfo::ImageInfo { shape, .. } => shape,
+        _ => anyhow::bail!("Primary HDU is not an image"),
+    };
+
+    ensure!(shape.len() == 2, "expected 2D image");
+
+    let (h, w) = (shape[0] as usize, shape[1] as usize);
+
+    let data: Vec<f32> = hdu.read_image(&mut f)?;
+
+    Ok(FitsImage {
+        data,
+        width: w,
+        height: h,
+    })
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rapidfits_lib::fits::load_fits_f32;
+
 fn main() {
-    rapidfits_lib::run()
+    rapidfits_lib::run();
 }
